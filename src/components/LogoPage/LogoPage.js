53a27209fc1648afb72985295c6b72e6a5c07fb6
Nicolas Cabrera <nicolascabreramillacan@gmail.com>
2023-08-10 15:16:18 -0400

    feat: change logo in components/logoPage/logoPage.js

--- src/components/LogoPage/LogoPage.js
+++ src/components/LogoPage/LogoPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import LogoAulenPropiedades from '../../assets/img/LogoSite/LOGO-AULEN-PROPIEDADES.png';
+import LogoAulenPropiedades from '../../assets/img/LogoSite/logoAulen.png';
 import styles from '../../styles/Layout/Logo.module.css';
 
 const LogoPage = () => {
