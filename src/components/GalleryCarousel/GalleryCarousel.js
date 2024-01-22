30f51e6b0cafa02f3219657a5520b77175d1fc6a
fabian <fabian.salas.astete@gmail.com>
2024-01-22 10:39:45 -0300

    fix: carousel images not found fixed

--- src/components/GalleryCarousel/GalleryCarousel.js
+++ src/components/GalleryCarousel/GalleryCarousel.js
@@ -1,18 +1,58 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ImageGallery from 'react-image-gallery';
 
+import PropertiesServices from '../../services/PropertiesServices';
+import { company } from '../../constants/consts/company';
+
 const GalleryCarousel = ({ items }) => {
-  const getImages = () =>
-    items?.images
-      ? items?.images?.map((_, idx) => ({
-          original: `https://accion.panal.house//Imagenes//${
-            items?.id
-          }//${idx + 1}.jpg`,
-          thumbnail: `https://accion.panal.house//Imagenes//${
-            items?.id
-          }//${idx + 1}.jpg`,
-        }))
-      : [];
+const [itemsId, setItemsId]=useState("");
+
+const getPropertyId = async (id, statusId, companyId)=>{
+  const data = await PropertiesServices.getProperty(id, statusId, companyId);
+
+  setItemsId(data);
+}
+
+
+useEffect(()=>{
+  getPropertyId(items.id, 1, company.companyId)
+}, [])
+
+  // const getImages = () =>
+  //   items?.images
+  //     ? items?.images?.map((_, idx) => ({
+  //         original: `https://accion.panal.house//Imagenes//${
+  //           items?.id
+  //         }//${idx + 1}.jpg`,
+  //         thumbnail: `https://accion.panal.house//Imagenes//${
+  //           items?.id
+  //         }//${idx + 1}.jpg`,
+  //       }))
+  //     : [];
+
+      const getImages = (images) =>
+      images
+        ? images?.map((_, idx) => {
+            const image = images[idx];
+            const validExtensions = ['.jpg', '.jpeg', '.png'];
+
+    
+            if (image && validExtensions.some(ext => image.toLowerCase().endsWith(ext))) {
+              return {
+                original: `${image }`,
+                thumbnail: `${image }`,
+              };
+            }
+    
+            // Devolver un objeto en caso si la extensión no es válida
+            return {
+              original:`https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg`,
+              thumbnail:`https://res.cloudinary.com/dbrhjc4o5/image/upload/v1681933697/unne-media/errors/not-found-img_pp5xj7.jpg`
+            }
+       ;
+          })
+        : [];
+
 
   return (
     <ImageGallery
@@ -26,7 +66,7 @@ const GalleryCarousel = ({ items }) => {
       showBullets={false}
       showThumbnails={true}
       thumbnailPosition="left"
-      items={getImages()}
+      items={getImages(itemsId?.images)}
     />
   );
 };
